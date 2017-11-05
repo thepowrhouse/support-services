db9a7521e48a324ac8c39c2193cf235eb09b3a93
Sujit Deshpande <sujit.deshpande@ness.com>
2017-11-06 01:31:28 +0530

    First Commit

---
+++ services/hystrix-dashboard/src/main/java/com/fsd/supportservices/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,31 @@
+package com.fsd.supportservices.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+@EnableHystrixDashboard
+@EnableDiscoveryClient
+public class HystrixDashboardApplication extends SpringBootServletInitializer {
+
+	@RequestMapping("/")
+	public String home() {
+		return "forward:/hystrix";
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HystrixDashboardApplication.class).web(true);
+	}
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(HystrixDashboardApplication.class).web(true).run(args);
+	}
+}
